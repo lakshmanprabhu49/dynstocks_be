ddef9011cf07d609befba3d4b98fe7eb18e5dcf8
lakshmanprabhu49 <lakshmanprabhu49@gmail.com>
2023-10-09 22:42:23 +0530

    Completed PUT Real Time Price

--- src/APIs/dynStocksRealTimePrice.py
+++ src/APIs/dynStocksRealTimePrice.py
@@ -54,13 +54,14 @@ class DynStocksRealTimePrice(Resource):
 
             return "User details created successfully", 201
 
-    def put(self, userId, stockCode):
+    def put(self, userId):
         # PUT localMaxima and localMimima
         checkXRequestIdHeader(request= request)
         if ('Authorization' not in request.headers):
             abort(403, message = 'You are unauthorized to make the request') 
         jwt_token = request.headers["Authorization"].split(" ")
         access_code = request.args.get('accessCode', default=None)
+        print(os.environ.get("accessCode " + userId))
         if (access_code != os.environ.get("accessCode " + userId)):
             abort(403, message = 'Please enter a valid Access Code')
         expected_jwt_token = jwt.encode({
@@ -68,6 +69,33 @@ class DynStocksRealTimePrice(Resource):
         }, os.environ.get("DYNSTOCKS_SECRET"), algorithm=os.environ.get('DYNSTOCKS_JWT_ALGO') )
         if (len(jwt_token) != 2 or jwt_token[1] != expected_jwt_token):
             abort(403, message = 'You are unauthorized to make the request')
+        content_type = request.headers['content-type']
+        if ('application/json' in content_type):
+            body = request.get_json()
+            stockDetails = body['stockDetails']
+            requiredUserDetails = mongoDB_DynStocks_RealTime_Price_Collection.find_one({
+                'userId': userId,
+            })
+            if (not requiredUserDetails):
+                abort(404, "User not found")
+            res = None
+            updatedStockDetails = stockDetails
+            res = mongoDB_DynStocks_RealTime_Price_Collection.find_one_and_update({
+                'userId': userId
+            }, {
+                '$set': {
+                    'userId': userId,
+                    'stockDetails': updatedStockDetails
+                }
+            })
+            if (not res):
+                abort(400, "Error while updating the real time prices")
+            return {
+                'userId': userId,
+                'stockDetails': updatedStockDetails
+            }, 200
+
+
 
     def delete(self, userId):
         # DELETE userId
